221f98645225e8612a06bb712fc322d09da33eea
Brock Whitten <brock@chloi.io>
2016-11-04 11:34:14 -0700

    adds first microplatform code

---
+++ test/basic.js
@@ -0,0 +1,11 @@
+var microplatform = require("../")
+var should = require("should")
+
+describe("microplatform", function(){
+  
+  it("should exist", function(done){
+    should.exist(microplatform)
+    done()
+  })
+
+})
